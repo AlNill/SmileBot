52a5208ce08edf97448c8b2f6350a8049c9fe740
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-28 14:17:03 -0700

    Info from created bot

---
+++ SmileBotFirstConnection/Program.cs
@@ -0,0 +1,6 @@
+﻿using Telegram.Bot;
+
+var botClient = new TelegramBotClient("{YOUR_ACCESS_TOKEN_HERE}");
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am bot {me.Id} and my name is {me.FirstName}.");
