41bbd97ac89742887525138730ce284ef4159aaa
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-28 14:09:16 -0700

    Revert "First get info from bot"
    This reverts commit d4fcc13f166a889d48975dd9d70a32865ba0ce3c.

--- SmileBotFirstConnection/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Telegram.Bot;
-
-var botClient = new TelegramBotClient("Your_token");
-
-var me = await botClient.GetMeAsync();
-Console.WriteLine($"Hello, World! I am bot {me.Id} and my name is {me.FirstName}.");
