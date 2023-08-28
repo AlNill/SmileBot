7002b9b193161eca90889d2cfb62d0b8d3a7ba05
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-28 14:14:39 -0700

    Remove unusable code

--- SmileBotFirstConnection/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Telegram.Bot;
-
-var botClient = new TelegramBotClient("TOKEN");
-
-var me = await botClient.GetMeAsync();
-Console.WriteLine($"Hello, World! I am bot {me.Id} and my name is {me.FirstName}.");
