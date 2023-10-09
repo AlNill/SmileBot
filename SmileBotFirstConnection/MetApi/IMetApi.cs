143a4e8b20c2579e92d6cd04956fbdb9f0c1d1c4
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-09 13:26:46 -0700

    Pulling Data from the Met Collection API

---
+++ SmileBotFirstConnection/MetApi/IMetApi.cs
@@ -0,0 +1,10 @@
+﻿using SmileBotFirstConnection.Models;
+
+namespace SmileBotFirstConnection.MetApi;
+
+public interface IMetApi
+{
+    Task<CollectionObjects> GetCollectionObjectsAsync();
+    Task<CollectionItem> GetCollectionItemAsync(string objectNum);
+    Task<CollectionObjects> SearchCollectionAsync(string query);
+}
---
+++ SmileBotFirstConnection/Models/CollectionObjects.cs
@@ -0,0 +1,7 @@
+﻿namespace SmileBotFirstConnection.Models;
+
+public class CollectionObjects
+{
+    public int total { get; set; }
+    public List<int>? objectIDs { get; set; }
+}
